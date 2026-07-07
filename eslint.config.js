183f059a2a79a038d206142518ec0d0fbf95b039
Anas Raza <anas@geeksofkolachi.com>
2026-07-07 14:42:22 +0500

    chore: configure eslint, prettier, husky, lint-staged, and commitlint

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+const { defineConfig, globalIgnores } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = defineConfig([
+  expoConfig,
+  eslintPluginPrettierRecommended,
+  globalIgnores(['dist/*', '.expo/*', 'expo-env.d.ts', 'scripts/reset-project.js']),
+]);
